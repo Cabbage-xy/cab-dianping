46abf700b2fe9cb4dbad93980f14c063654691c7
Cabbage-xy <cxy4915@gmail.com>
2024-05-20 22:10:46 +0800

    feat: login with code

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package com.hmdp.service.impl;
 
 import cn.hutool.core.util.RandomUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
@@ -9,10 +11,13 @@ import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexPatterns;
 import com.hmdp.utils.RegexUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.data.redis.connection.ReactiveClusterScriptingCommands;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -25,6 +30,8 @@ import javax.servlet.http.HttpSession;
 @Service
 @Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+    @Mapper
+    UserMapper userMapper;
 
     @Override
     public Result sendCode(String phone, HttpSession session) {
@@ -45,5 +52,50 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
 
         // 返回ok
         return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        // 0.校验手机号
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(loginForm.getPhone())){
+            // 如果不符合，返回错误信息
+            return Result.fail("手机号格式错误");
+        }
+        // 1. 校验验证码
+        Object cacheCode = session.getAttribute("code");
+        String code = loginForm.getCode();
+        if (cacheCode == null || !cacheCode.toString().equals(code)) {
+            return Result.fail("验证码错误");
+        }
+
+        // 2. 根据手机号查询
+        User user = query().eq("phone", phone).one();
+        // 另一种写法
+        // QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        // queryWrapper.eq("phone", phone);
+        // User user = userMapper.selectOne(queryWrapper);
+
+        // 3. 用户不存在
+        if (user == null) {
+            // 创建用户
+            user = createUserWithPhone(phone);
+        }
+
+        // 5. 保存到session
+        session.setAttribute("user", user);
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone) {
+        // 1. 创建用户
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+
+        // 2. 保存用户
+        save(user);
+
+        return user;
     }
 }
